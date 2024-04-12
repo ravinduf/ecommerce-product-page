a62614985baa4675a44927db1aee3ed30b72601f
ravinduf <ravinduf@rocketmail.com>
2024-04-12 23:39:47 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="content">
+    <h1 class="text-3xl">Rsbuild with Vue</h1>
+    <p>Start building amazing things with Rsbuild.</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
