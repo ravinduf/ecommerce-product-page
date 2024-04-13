d96dbe1074d97e974ee8fd8f69d7a1e0aaec8723
ravinduf <ravinduf@rocketmail.com>
2024-04-13 11:10:45 +0530

    setup tailwing configl

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'kumbh': ['Kumbh Sans', 'serif']
+      },
+      colors: {
+        primaryOrange: 'hsl(26, 100%, 55%)',
+        primaryPaleOrange: 'hsl(25, 100%, 94%)',
+        neutralDarkBlue: 'hsl(220, 13%, 13%)',
+        neutralGrayishBlue: 'hsl(220, 14%, 75%)',
+        neutralDarkGrayishBlue: 'hsl(219, 9%, 45%)',
+        neutralLightGrayishBlue: 'hsl(223, 64%, 98%)',
+      }
+    },
   },
   plugins: [],
 }
